b652929fb1c5ee358934a78e66a9d694f3c97301
Collin Broderick <cabroderick@wpi.edu>
2019-12-10 20:35:26 -0500

    Praise fix it

--- AlgoHW6/AlgoHW6.cpp
+++ AlgoHW6/AlgoHW6.cpp
@@ -32,12 +32,19 @@ vector<int> nextLegalPosition(vector<int> board, int n) {
 			if (isLegalPosition(board, n)) {
 				newPos = i;
 			} else newPos = i - 1;
-			for (int a = 0; a < n; a++) {
-				vector<int> newBoard = board;
-				newBoard[newPos] = a; // fill in position after last valid position
-				if (isLegalPosition(newBoard, n)) return newBoard;
+			for (int j = newPos; j > 0; j--) {
+				int newVal = board[j];
+				if (newVal != n) {
+					for (int a = newVal + 1; a <= n; a++) {
+						vector<int> newBoard = board;
+						newBoard[j] = a; // fill in position after last valid position
+						if (isLegalPosition(newBoard, n)) return newBoard;
+					}
+				}
+				// here the row must be emptied out
+				board[j] = 0;
 			}
-			break;
+			return vector<int>(n, 0);
 		}
 	}
 	// here the board must be full
@@ -52,14 +59,102 @@ vector<int> nextLegalPosition(vector<int> board, int n) {
 		}
 		board[i] = 0;
 	}
-	return vector<int>(n);
+	return vector<int>(n, 0); // dummy return will never happen I think
+}
+
+// Determines if a given board is full
+// board: The board
+// n: The size of the board
+// Returns true if full, false otherwise
+bool isFull(vector<int> board, int n) {
+	for (int i = 0; i < n; i++) {
+		if (board[i] == 0) return false; // if there are any 0s the board is not full
+	}
+	return true;
+}
+
+// Determines if a given board is empty
+// board: The board
+// n: The size of the board
+// Returns true if empty, false otherwise
+bool isEmpty(vector<int> board, int n) {
+	for (int i = 0; i < n; i++) {
+		if (board[i] != 0) return false; // if there are any non-0s the board is not empty
+	}
+	return true;
+}
+
+// Solves the n queens problem for a given board size
+// n: The board size
+// Returns a list of the completed boards
+vector<vector<int>> nQueens(int n) {
+	vector<vector<int>> solutions; // vector containing the solutions to the problem
+	
+	/*
+	for (int i = 1; i <= n; i++) {
+		vector<int> board(n, 0);
+		board[0] = i;
+		while (!isEmpty(board, n)) {
+			board = nextLegalPosition(board, n);
+			if (isFull(board, n)) solutions.push_back(board);
+		}
+		return solutions;
+	}
+	*/
+	
+	
+	
+	for (int i = 1; i <= n; i++) {
+		vector<int> board(n, 0);
+		board[0] = i; // initializes first row value
+		while (!isFull(board, n) && !isEmpty(board, n)) {
+			board = nextLegalPosition(board, n);
+		}
+		if (!isEmpty(board, n)) solutions.push_back(board);
+	}
+	return solutions;
+}
+
+
+// Prints solutions 4-15 for the n queens problem
+void printSolutions() {
+	for (int i = 4; i <= 15; i++) {
+		vector<vector<int>> solutions = nQueens(i);
+		int size = solutions.size();
+		cout << "The n queens problem has " << size << " solutions for n = " << i << ":" << endl;
+		for (int a = 0; a < size; a++) {
+			for (int b = 0; b < solutions[a].size(); b++) {
+				cout << solutions[a][b] << " ";
+			}
+			cout << endl;
+		}
+	}
 }
 
 int main() {
+	/*
 	vector<int> pos1 = { 1, 6, 8, 3, 7, 4, 2, 5 };
-	vector<int> pos2 = {1,6,8,3,5,0,0,0};
+	vector<int> pos2 = { 1, 6, 8, 3, 7, 0, 0, 0 };
+	vector<int> empty(8, 0);
+	vector<int> ans2 = nextLegalPosition(pos2, 8);
 	vector<int> ans1 = nextLegalPosition(pos1, 8);
 	for (int i = 0; i < 8; i++) {
 		cout << ans1[i] << " ";
 	}
+	cout << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << ans2[i] << " ";
+	}
+	cout << endl;
+	//vector<vector<int>> four = nQueens(4);
+	vector<vector<int>> eight = nQueens(15);
+	for (int i = 0; i < eight.size(); i++) {
+		for (int j = 0; j < eight[i].size(); j++) {
+			cout << eight[i][j] << " ";
+		}
+		cout << endl;
+	}
+	*/
+	vector<vector<int>> sol = nQueens(4);
+
 }
